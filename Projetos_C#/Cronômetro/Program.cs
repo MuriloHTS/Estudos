7d8bb6c118595910a0ed53588fd991b212a01044
Murilo Henrique <murilosilva21@hotmail.com>
2022-10-21 11:56:14 -0300

    Cronômetros

---
+++ Projetos_C#/Cronômetro/Program.cs
@@ -0,0 +1,60 @@
+﻿menu();
+
+static void menu()
+{
+    Console.Clear();
+    Console.WriteLine("Deseja a contagem em minutos ou em segundos?");
+    Console.WriteLine("1 - Minutos");
+    Console.WriteLine("2 - Segundos");
+    Console.WriteLine("3 - Sair");
+    Console.WriteLine("Escolha uma opção: ");
+
+    int escolha = int.Parse(Console.ReadLine());
+    
+    switch(escolha)
+    {
+        case 1: StartM(); break; 
+        case 2: StartS(); break;
+        default: System.Environment.Exit(0); break;
+    }
+}
+
+static void StartM()
+{
+    int currentTime = 0;
+
+    Console.WriteLine("Digite o tempo desejado em minutos: ");
+    int time = int.Parse(Console.ReadLine());
+
+    while(currentTime != time * 60)
+    {
+        Console.Clear();
+        currentTime++; 
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.WriteLine("Cronômetro finalizado!");
+    Thread.Sleep(2000);
+    menu();
+}
+
+static void StartS()
+{
+    int currentTime = 0;
+
+    Console.WriteLine("Digite o tempo desejado em segundos: ");
+    int time = int.Parse(Console.ReadLine());
+
+    while(currentTime != time)
+    {
+        Console.Clear();
+        currentTime++; 
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.WriteLine("Cronômetro finalizado!");
+    Thread.Sleep(2000);
+    menu();
+}
---
+++ Projetos_C#/Cronômetro_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Start();
+
+static void Start()
+{
+    string tempinho = Console.ReadLine();
+    int tempao = int.Parse(tempinho.Substring(tempinho.Length - 6, 2));
+
+    Console.Clear();
+
+    Console.WriteLine("Digite o tempo separado por ':' - ");
+
+    Console.WriteLine($"{tempao}");
+}
